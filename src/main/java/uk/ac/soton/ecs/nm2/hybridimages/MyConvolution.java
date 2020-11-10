81366e5c3305c66aa82546efbdcc7181de059a7c
Nawab <nawab.mir14@gmail.com>
2020-11-10 23:41:01 +0000

    added convolution to processImage

--- src/main/java/uk/ac/soton/ecs/nm2/hybridimages/MyConvolution.java
+++ src/main/java/uk/ac/soton/ecs/nm2/hybridimages/MyConvolution.java
@@ -1,5 +1,6 @@
 package uk.ac.soton.ecs.nm2.hybridimages;
 
+import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.processor.SinglebandImageProcessor;
 
@@ -14,10 +15,26 @@ public class MyConvolution implements SinglebandImageProcessor<Float, FImage> {
     // Convolve image with kernel and store result back in image
     @Override
     public void processImage(FImage image) {
+        int kernelHeight = this.kernel.length;
+        int kernelWidth = this.kernel[0].length;
+
         // Pad the image to avoid reducing it's dimensions once convolved
-        int heightToPad = this.kernel.length;
-        int widthToPad = this.kernel[0].length;
-        FImage tempImage = new FImage(image.width + widthToPad, image.height + heightToPad);
+        int heightToPad = (int)Math.floor(kernelHeight / 2);
+        int widthToPad = (int)Math.floor(kernelWidth / 2);
+        FImage tempImage = new FImage(image.getHeight() + widthToPad, image.getWidth() + heightToPad);
         tempImage.internalAssign(image.padding(widthToPad, heightToPad, 0f));
+
+        // Apply kernel to image
+        for(int y = 0; y < image.getHeight() ; y++ ){
+            for(int x = 0; x < image.getWidth(); x++) {
+                float sum = 0;
+                for(int ky = 0; ky < kernelHeight; ky++){
+                    for(int kx = 0; kx < kernelWidth; kx++) {
+                        sum += this.kernel[ky][kx] * tempImage.pixels[y + ky][x + kx];
+                    }
+                }
+                image.pixels[y][x] = sum;
+            }
+        }
     }
 }
