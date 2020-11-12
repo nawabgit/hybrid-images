e862fd4d833104db576f710f6888e63677bf4350
Nawab <nawab.mir14@gmail.com>
2020-11-12 23:44:56 +0000

    added extra useful comments

--- src/main/java/uk/ac/soton/ecs/nm2/hybridimages/MyConvolution.java
+++ src/main/java/uk/ac/soton/ecs/nm2/hybridimages/MyConvolution.java
@@ -1,6 +1,5 @@
 package uk.ac.soton.ecs.nm2.hybridimages;
 
-import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.processor.SinglebandImageProcessor;
 
@@ -8,7 +7,9 @@ public class MyConvolution implements SinglebandImageProcessor<Float, FImage> {
     private float[][] kernel;
 
     public MyConvolution(float[][] kernel) {
-        // flip the kernel
+        // Flip the kernel
+        // If we do not flip the kernel we are not applying the convolution operator
+        // But actually the correlation operator
         float [][] flippedKernel = kernel.clone();
 
         for(int i = 0; i < kernel.length; i++){
@@ -27,6 +28,8 @@ public class MyConvolution implements SinglebandImageProcessor<Float, FImage> {
         int kernelWidth = this.kernel[0].length;
 
         // Pad the image to avoid reducing it's dimensions once convolved
+        // We only need to pad each side by half since only half of the kernel will protrude
+        // Out of the range of the original image
         int heightToPad = (int)Math.floor(kernelHeight / 2);
         int widthToPad = (int)Math.floor(kernelWidth / 2);
         FImage tempImage = new FImage(image.getHeight() + widthToPad, image.getWidth() + heightToPad);
@@ -38,9 +41,11 @@ public class MyConvolution implements SinglebandImageProcessor<Float, FImage> {
                 float sum = 0;
                 for(int ky = 0; ky < kernelHeight; ky++){
                     for(int kx = 0; kx < kernelWidth; kx++) {
+                        // Make sure to apply to the padded image so we can retain original dimensions
                         sum += this.kernel[ky][kx] * tempImage.pixels[y + ky][x + kx];
                     }
                 }
+                // Pixel is the sum of all nearby pixels that the kernel covers (of padded image)
                 image.pixels[y][x] = sum;
             }
         }
--- src/main/java/uk/ac/soton/ecs/nm2/hybridimages/MyHybridImages.java
+++ src/main/java/uk/ac/soton/ecs/nm2/hybridimages/MyHybridImages.java
@@ -1,6 +1,5 @@
 package uk.ac.soton.ecs.nm2.hybridimages;
 
-import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.MBFImage;
 import org.openimaj.image.processing.convolution.Gaussian2D;
 
@@ -20,15 +19,7 @@ public class MyHybridImages {
      * @return the computed hybrid image
      */
     public static MBFImage makeHybrid(MBFImage lowImage, float lowSigma, MBFImage highImage, float highSigma) {
-        //implement your hybrid images functionality here.
-        //Your submitted code must contain this method, but you can add
-        //additional static methods or implement the functionality through
-        //instance methods on the `MyHybridImages` class of which you can create
-        //an instance of here if you so wish.
-        //Note that the input images are expected to have the same size, and the output
-        //image will also have the same height & width as the inputs.
-
-        // This will be our low version of our high frequency image which we will subtract from the original
+        // This will be the low version of our high frequency image which we will subtract from the original
         // To produce a high frequency image
         MBFImage highImageCopy = highImage.clone();
 
@@ -48,6 +39,7 @@ public class MyHybridImages {
         return hybridImage;
     }
 
+    // Helper method to produce a Gaussian kernel
     private static float[][] createGaussianKernel(float sigma){
         int size = (int) (8.0f * sigma + 1.0f); // (this implies the window is +/- 4 sigmas from the centre of the Gaussian)
         if (size % 2 == 0) size++; // size must be odd
